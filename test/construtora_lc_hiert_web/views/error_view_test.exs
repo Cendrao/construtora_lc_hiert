02c9ece2fd2d7a54825111881d15fc8fceb9e4ed
Ricardo Ruwer <ricardoruwer@gmail.com>
2019-06-03 17:20:04 -0300

    Add 404 and 500 page :balloon::house: (#42)

--- test/construtora_lc_hiert_web/views/error_view_test.exs
+++ test/construtora_lc_hiert_web/views/error_view_test.exs
@@ -5,10 +5,12 @@ defmodule ConstrutoraLcHiertWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.html" do
-    assert render_to_string(ConstrutoraLcHiertWeb.ErrorView, "404.html", []) == "Not Found"
+    assert render_to_string(ConstrutoraLcHiertWeb.ErrorView, "404.html", []) =~
+             "Página não encontrada"
   end
 
   test "renders 500.html" do
-    assert render_to_string(ConstrutoraLcHiertWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(ConstrutoraLcHiertWeb.ErrorView, "500.html", []) =~
+             "Ocorreu um problema"
   end
 end
