655c5f8b73b5dbcf614d97042fdbd0681902d148
Ricardo Ruwer <ricardoruwer@gmail.com>
2019-07-30 13:18:21 -0300

    Improve front-end :art:

--- lib/construtora_lc_hiert_web/views/admin/property/image_view.ex
+++ lib/construtora_lc_hiert_web/views/admin/property/image_view.ex
@@ -1,7 +1,3 @@
 defmodule ConstrutoraLcHiertWeb.Admin.Property.ImageView do
   use ConstrutoraLcHiertWeb, :view
-
-  def image_url(image, opts) do
-    ConstrutoraLcHiertWeb.PropertyView.image_url(image, opts)
-  end
 end
