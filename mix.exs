8c007f7a49619c179c88bdd3d9cb76dec1c977f3
Ricardo Ruwer <ricardoruwer@gmail.com>
2019-10-02 12:57:25 -0300

    Add tool-versions (#104)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ConstrutoraLcHiert.MixProject do
     [
       app: :construtora_lc_hiert,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
